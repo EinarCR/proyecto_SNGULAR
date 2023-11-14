c6658839df50c853a130f18f338901fab8efaee9
EinarCR <einar_cordova@hotmail.com>
2023-11-14 15:16:50 -0600

    Se agregan pruebas unitarias

--- src/app/logica/logica.component.spec.ts
+++ src/app/logica/logica.component.spec.ts
@@ -22,4 +22,36 @@ describe('LogicaComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+    it('Debería calcular correctamente la sucesión de Fibonacci', () => {
+    expect(component.fibonacci(0)).toBe(0);
+    expect(component.fibonacci(1)).toBe(1);
+    expect(component.fibonacci(2)).toBe(1);
+    expect(component.fibonacci(5)).toBe(5);
+  });
+
+  it('Debería calcular correctamente el número triangular', () => {
+    expect(component.triangular(1)).toBe(1);
+    expect(component.triangular(2)).toBe(3);
+    expect(component.triangular(3)).toBe(6);
+  });
+
+  it('Debería obtener correctamente el número primo en una posición', () => {
+    expect(component.obtenerPrimoEnPosicion(1)).toBe(2);
+    expect(component.obtenerPrimoEnPosicion(2)).toBe(3);
+    expect(component.obtenerPrimoEnPosicion(3)).toBe(5);
+  });
+
+  it('Debería validar correctamente si el número es primo', () => {
+    expect(component.esPrimo(1)).toBe(false);
+    expect(component.esPrimo(2)).toBe(true);
+    expect(component.esPrimo(4)).toBe(false);
+  });
+
+  it('Debería calcular correctamente la serie', () => {
+    expect(component.serie(0)).toBe(0);
+    expect(component.serie(1)).toBe(0.5);
+    expect(component.serie(2)).toBe(0.14285714285714285);
+    expect(component.serie(5)).toBe(0.045454545454545456);
+  });
 });
